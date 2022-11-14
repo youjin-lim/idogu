cadeffbb6453930d50f214b8d39f3a32bcf1643a
youjin-lim <110709781+youjin-lim@users.noreply.github.com>
2022-11-14 11:35:37 +0900

    Add files via upload
    rename pro

---
+++ idogu_1/idogu/src/main/java/net/member/action/MemberViewAction.java
@@ -0,0 +1,48 @@
+package net.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.member.db.MemberBean;
+import net.member.db.MemberDAO;
+// 회원이 내정보 보기를 눌렀을떄 
+public class MemberViewAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		ActionForward forward = new ActionForward();
+		
+		HttpSession session = request.getSession();
+		MemberDAO memberdao = new MemberDAO();
+		MemberBean member = new MemberBean();
+		
+	    session.setAttribute("id",  "korea"); //테스트용 작성줄 
+		
+		String id = (String) session.getAttribute("id");
+		
+		if(id == null) {
+			forward.setRedirect(true);
+			forward.setPath("./MemberLogin.me");
+		}
+		
+		
+		member = memberdao.getDetailMember(id);
+		
+		if(member == null) {
+			System.out.println("회원 정보 불러오기 실패");
+			return null;
+		}
+		
+		request.setAttribute("member", member);
+		
+		forward.setRedirect(false);
+		forward.setPath("./member/memberform.jsp");
+		
+		
+		
+		return forward;
+	}
+
+}
